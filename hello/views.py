0d58906883a65d18f277df472575505392251891
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-06-02 12:18:12 +0900

    HTMLの更新

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
 
-class HelloView(TemplateView):
 
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'message': 'your data',
-            'form': HelloForm(),
-            'result': None
-        }
-    
-    def get(self,request):
-        return render(request,'hello/index.html',self.params)
-    
-    def post(self,request):
-        ch = request.POST['choice']
-        self.params['result'] = 'selected: ' + ch + '.'
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
-# Create your views here.
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': 'all friends.',
+        'data': data,
+    }
+    return render(request, 'hello/index.html', params)
