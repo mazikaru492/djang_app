ccb118af3c21be41edf69e18b2e2e0652661f831
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-06-09 11:24:17 +0900

    3-28

--- hello/forms.py
+++ hello/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class HelloForm(forms.Form):
     name = forms.CharField(label='name')
     mail = forms.EmailField(label='mail')
-    gendeir = forms.BooleanField(label='gender', required=False)
+    gender = forms.BooleanField(label='gender', required=False)
     age = forms.IntegerField(label='age')
     birthday = forms.DateField(label='Birth')
--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Friend
 from django.db.models import QuerySet
-
-
-def __new_str__(self):
-    result = ""
-    for item in self:
-        result += '<tr>'
-        for k in item:
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
-        result += '</tr>'
-    return result
-
-
-QuerySet.__str__ = __new_str__
+from .forms import HelloForm
 
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
         'data': data,
     }
     return render(request, 'hello/index.html', params)
+#crate model
+def create(request):
+    params = {
+        'title': 'Hello',
+        'form': HelloForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST.get('name')
+        mail = request.POST.get('mail')
+        gender = 'gender' in request.POST
+        age = int(request.POST.get('age'))
+        birth = request.POST.get('birthday')
+        dirth = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+        dirth.save()
+        return redirect('/hello')
+    return render(request, 'hello/create.html', params)
