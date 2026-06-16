8258016f7641a41079af919513642928a7c36ed8
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-06-16 09:44:37 +0900

    3.5

--- hello/views.py
+++ hello/views.py
@@ -56,7 +56,7 @@ def find(request):
         msg = 'search result:'
         form = FindForm(request.POST)
         str = request.POST['find']
-        data = Friend.objects.filter(name=str)
+        data = Friend.objects.filter(name__iexact=str)
     else:
         msg = 'search words...'
         form = FindForm()
