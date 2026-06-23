45d5063492c9ecc3e8362eee00f1638a3a05dd45
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-06-23 09:55:28 +0900

    4-31

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import ValidationError
 
 def number_only(value):
-    if (re.match(r'^[0-9]*$', value) == None):
+    if (re.match(r'^[0-9]*$', value) is None):
         raise ValidationError(
             '%(value)s is not Number!',
             params={'value': value},
@@ -20,3 +20,16 @@ class Friend(models.Model):
 	def __str__(self):
 		return '<Friend:id=' + str(self.id) + ', ' + \
 			self.name + '(' + str(self.age) + ')>'
+
+class Message(models.Model):
+	friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	content = models.CharField(max_length=300)
+	pub_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '<Message:id=' + str(self.id) + ', ' + \
+			self.title + '(' + str(self.pub_date) + ')>'
+
+	class Meta:
+		ordering = ('pub_date',)
