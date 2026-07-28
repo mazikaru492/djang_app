6581a0e30900e8145b29b8c4a75fd2628f83091c
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-07-28 11:48:12 +0900

    テスト問題

--- django_skills_test/django_skills_test/urls.py
+++ django_skills_test/django_skills_test/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djang_app project.
+URL configuration for django_skills_test project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from syllabus.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", include("hello.urls")),
-    path("sns/", include("sns.urls")),
-    path("book/", include("book.urls")),
-    path("syllabus/", include("syllabus.urls")),
+    path('', index, name='index'),
 ]
