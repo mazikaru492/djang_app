45e4ef6813443d139d52c9d082f7eb4383ee3d78
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-08-02 17:19:38 +0900

    要件9

--- django_skills_test/syllabus/views.py
+++ django_skills_test/syllabus/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Course
 
 def index(request):
-    course_list = Course.objects.all()
+    keyword = request.GET.get('keyword', '')
+    if keyword:
+        course_list = Course.objects.filter(
+            Q(title__icontains=keyword) | Q(teacher__icontains=keyword)
+        )
+    else:
+        
+        course_list = Course.objects.all()
     paginator = Paginator(course_list, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'syllabus/index.html', {'page_obj': page_obj})
+    context = {
+        'title': '講義シラバス検索',
+        'message': '講義名または担当教員名で検索できます。',
+        'keyword': keyword,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'syllabus/index.html', context)
