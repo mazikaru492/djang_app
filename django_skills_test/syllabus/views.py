d2282406edf05a01032c011833f55250fec355a4
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-08-02 17:13:13 +0900

    要件8

--- django_skills_test/syllabus/views.py
+++ django_skills_test/syllabus/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import Course
 
 def index(request):
-    return render(request, 'syllabus/index.html')
+    course_list = Course.objects.all()
+    paginator = Paginator(course_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'syllabus/index.html', {'page_obj': page_obj})
