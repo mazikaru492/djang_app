9baaabdb35163cf8b0035668e7f027cfc2d3f079
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-08-03 19:55:01 +0900

    要件11終わり

--- django_skills_test/syllabus/views.py
+++ django_skills_test/syllabus/views.py
@@ -11,24 +11,24 @@ def index(request):
         )
     else:
         course_list = Course.objects.all()
-
     paginator = Paginator(course_list, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
     context = {
         'title': '講義シラバス検索',
         'message': '講義名または担当教員名で検索できます。',
         'keyword': keyword,
         'page_obj': page_obj,
     }
-
     return render(request, 'syllabus/index.html', context)
+
 
 def detail(request, id):
     course = get_object_or_404(Course, id=id)
+    reviews = course.review_set.all().order_by('-created_at')
     context = {
         'title': '講義詳細（シラバス）',
         'course': course,
+        'reviews': reviews,
     }
     return render(request, 'syllabus/detail.html', context)
